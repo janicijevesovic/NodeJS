2f7fa2bcd102e67f112f627f8c3e2c2843427030
janicijevesovic <janicije95@hotmail.com>
2022-10-18 19:00:44 +0200

    NodeJS - Start

---
+++ 01_Node_Tutorial/1-intro.js
@@ -0,0 +1,15 @@
+const amount = 9;
+
+if (amount < 10) {
+	console.log("Small number");
+} else {
+	console.log("Large number");
+}
+
+console.log(`Hey it's my first node app!!!`);
+
+for (i = 0; i < 5; i++) {
+	if (i % 2 === 0) {
+		console.log(i);
+	}
+}
