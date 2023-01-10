92f4d118c6bb3443b2771a6cdc85bd4c5a316c2a
janicijevesovic <janicije95@hotmail.com>
2023-01-10 19:08:32 +0100

    Modules - Alternative Syntax

--- 01_Node_Tutorial/app.js
+++ 01_Node_Tutorial/app.js
@@ -4,6 +4,10 @@ const names = require('./4-names');
 
 const sayHi = require('./5-utils');
 
+const data = require('./6-alternative-syntax');
+require('./7-interesting-example'); // Function gets executed
+
 sayHi(names.john);
 sayHi('Susan');
 sayHi(names.jim);
+console.log(data);
