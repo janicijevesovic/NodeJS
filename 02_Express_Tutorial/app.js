c60a9bcea237f611be0109eb2072d7aa7d9ef477
janicijevesovic <janicije95@hotmail.com>
2023-01-23 19:19:41 +0100

    HTTP App - Example

--- 02_Express_Tutorial/app.js
+++ 02_Express_Tutorial/app.js
@@ -1,11 +1,38 @@
 const http = require('http');
+const { readFileSync } = require('fs');
+
+// Get All Files
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
+
 const server = http.createServer((req, res) => {
 	// console.log(req.method);
 	const url = req.url;
+	console.log(url);
 	// Home Page
 	if (url === '/') {
 		res.writeHead(200, { 'content-type': 'text/html' });
-		res.write('<h1>Home Page</h1>');
+		res.write(homePage);
+		res.end();
+	}
+	// Styles
+	else if (url === '/styles.css') {
+		res.writeHead(200, { 'content-type': 'text/css' });
+		res.write(homeStyles);
+		res.end();
+	}
+	// Logo
+	else if (url === '/logo.svg') {
+		res.writeHead(200, { 'content-type': 'image/svg+xml' });
+		res.write(homeImage);
+		res.end();
+	}
+	// Logic
+	else if (url === '/browser-app.js') {
+		res.writeHead(200, { 'content-type': 'text/javascript' });
+		res.write(homeLogic);
 		res.end();
 	}
 	// About Page
