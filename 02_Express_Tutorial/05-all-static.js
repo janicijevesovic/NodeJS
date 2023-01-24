5a513967501932eb0ec7c72421f9248b2d7ecdcc
janicijevesovic <janicije95@hotmail.com>
2023-01-24 21:26:37 +0100

    Express App - Basics

---
+++ 02_Express_Tutorial/05-all-static.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Setup Static and Middleware
+app.use(express.static('./public'));
+
+// app.get('/', (req, res) => {
+// 	console.log(__dirname);
+// 	res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+//  Adding to static assets
+//  SSR
+// });
+
+app.all('*', (req, res) => {
+	res.status(404).send('Resource not found');
+});
+
+app.listen(5000, () => {
+	console.log('Server is listening on port 5000....');
+});
