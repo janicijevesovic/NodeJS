22cd1d130888221d8b9c2a30ff639b0d5f0a17ba
janicijevesovic <janicije95@hotmail.com>
2023-02-28 19:42:49 +0100

    Controllers & Routes, PostMan setup

--- 03_Task_Manager/controllers/tasks.js
+++ 03_Task_Manager/controllers/tasks.js
@@ -3,9 +3,25 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = (req, res) => {
-	res.send('Create Task');
+	res.json(req.body);
+};
+
+const getTask = (req, res) => {
+	res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+	res.send('Update Task');
+};
+
+const deleteTask = (req, res) => {
+	res.send('Delete Task');
 };
 
 module.exports = {
 	getAllTasks,
+	createTask,
+	getTask,
+	updateTask,
+	deleteTask,
 };
