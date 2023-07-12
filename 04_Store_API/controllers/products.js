bac1006910e0ab6172ef99b8bcf58775e8e31d02
janicijevesovic <janicije95@hotmail.com>
2023-07-12 19:08:51 +0200

    Mongoose | Sort and Fields Filter

--- 04_Store_API/controllers/products.js
+++ 04_Store_API/controllers/products.js
@@ -1,22 +1,34 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-	const search = 'ab';
-	const products = await Product.find({
-		name: { $regex: search, $options: 'i' },
-	});
+	const products = await Product.find({}).select('name price');
 	res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-	const { featured, company, name } = req.query;
+	const { featured, company, name, sort, fields } = req.query;
 	const queryObject = {};
 
 	if (featured) queryObject.featured = featured === 'true' ? true : false;
 	if (company) queryObject.company = company;
 	if (name) queryObject.name = { $regex: name, $options: 'i' };
 
-	const products = await Product.find(queryObject);
+	let result = Product.find(queryObject);
+	// Sort
+	if (sort) {
+		const sortList = sort.split(',').join(' ');
+		result = result.sort(sortList);
+	} else {
+		result = result.sort('createdAt');
+	}
+
+	//Fields
+	if (fields) {
+		const fieldsList = fields.split(',').join(' ');
+		result = result.select(fieldsList);
+	}
+
+	const products = await result;
 	res.status(200).json({ products, nbHits: products.length });
 };
 
