0b71e1364d5ab10bd0c5883e8db82afa72f0b921
janicijevesovic <janicije95@hotmail.com>
2023-11-06 19:23:30 +0100

    Dependencies Fix

--- 06_Jobs_API/app.js
+++ 06_Jobs_API/app.js
@@ -4,7 +4,6 @@ require('express-async-errors');
 // extra security packages
 const helmet = require('helmet');
 const cors = require('cors');
-const xss = require('xss');
 const rateLimiter = require('express-rate-limit');
 
 const express = require('express');
@@ -32,7 +31,6 @@ app.use(
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
-app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
