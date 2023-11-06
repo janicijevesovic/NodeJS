b2f366f7dde6956e198e8d1cdc657d8db2251386
janicijevesovic <janicije95@hotmail.com>
2023-11-06 19:13:05 +0100

    Security, Updated dependencies

--- 06_Jobs_API/app.js
+++ 06_Jobs_API/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -15,8 +22,17 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(
+	rateLimiter({
+		windowMs: 10 * 60 * 1000,
+		max: 20,
+	})
+);
+
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
